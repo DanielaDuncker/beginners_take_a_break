407aea4865195c938de8b6a7645273163f8d736f
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-27 10:28:37 +0200

    i tried and it almost worked

--- beginners_take_a_break.py
+++ beginners_take_a_break.py
@@ -5,10 +5,18 @@ import webbrowser
 total_breaks = 7
 break_count = 0
 
-print("This program started on "+time.ctime())
+print("This program started on " + time.ctime())
 while break_count < total_breaks:
     time.sleep(3600)
     webbrowser.open("https://www.youtube.com/watch?v=ZbZSe6N_BXs")
     break_count = break_count + 1
 
-schedule.every().day.at("09:00").do(break_count)
+schedule.every().day.at("09:00").do()
+while True:
+    schedule.run_pending()
+
+# WHY IS YOU NOT WORKING??!??!?!?!??!
+
+
+
+
